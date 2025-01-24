75190c5806cac8626695c5cdcaddca23562ad8d3
Naeem Islam <naeemislam.hasan74@gmail.com>
2025-01-25 02:11:24 +0600

    commiting banner contenr

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+// @import "tailwindcss";
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+  ],
+})
